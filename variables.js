5b7954bc94f01ae1e6803e77c30d28c183ab62e6
Luis Alvarado <luafer95@gmail.com>
2021-04-21 12:00:34 -0600

    prueba2

---
+++ variables.js
@@ -0,0 +1,24 @@
+// pasar el valor de una variable a otra
+var variable1 = 3;
+var variable2 = variable1;
+// alert(variable2);
+
+// Suma de 2 valores
+var num1 = 10;
+var num2 = 3;
+var suma = num1 + num2;
+var resta = num1 - num2;
+var multi = num1 * num2;
+var division = num1 / num2;  
+
+var num3 = 2;
+// num3 = num3 +1; esta linea significa lo mismo que la siguiente linea solo que mas larga
+num3 +=2;
+// Esto con las suma se pone el simbolo +, para las otras operaciones seria -, *, /.
+// alert(num3);
+
+// var modulo = num1  % num2;
+var modulo = num1 % 2;
+alert(modulo)
+
+
