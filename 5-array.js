b4c088ee19f3ca475deb0074765c0719a5579f57
Luis Alvarado <luafer95@gmail.com>
2021-04-28 15:30:24 -0600

    condicional

---
+++ 5-array.js
@@ -0,0 +1,14 @@
+var array= ['Verde', 'Amarillo','Rojo'];
+
+// El array puede almacenar diferentes tipos de valores/variables
+let miArray= ['Negro', 50, false];
+
+let miArray2= ['Negro', 50, false,true,null];
+
+let arrayNum= [1,2,3]
+let suma=arrayNum[2] + 10;
+arrayNum = [];//para vaciar el array y borrar sus valores dentro se iguala a parentesis cuadrados vacios.
+
+// se pude poner un array dentro de otro array de la siguiente manera.
+let arrayMulti=[[1,2,3],['hola','adios']]
+alert(arrayMulti[1][0])
---
+++ 6-condicionalIf.js
@@ -0,0 +1,20 @@
+var miVariable = 9;
+var condicional = miVariable == 10;
+var condicional2 = miVariable > 5;
+
+var edad=0;
+nombre='jose';
+if(nombre){
+    alert('Se cumple la condicion')
+}else{
+    alert('No se a cumplido la condicion')
+}
+
+
+// para concatenar se puede utilizar && que se deben cumplir todas la condicones.
+//  tambien exite or " || " que se necesita una de la condiciones para que se cumpla la condicion.
+// < > <= >= == !=
+// negacion " ! ", la negacion lo que hace es converti un falso en un verdadero y un verdadero en falso.
+
+// NOTA: si se pone un valor Positivo o Negativo, javascript lo interpreta como un valor verdadero pero si
+// el numero es cero entonces lo interpreta como  falso
